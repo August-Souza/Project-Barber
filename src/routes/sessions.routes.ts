97b987b51457475a55c15cc32f63b7dd94eecbbd
August-Souza <7luna.lunar@gmail.com>
2020-09-08 17:18:42 -0300

    exception handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import CreateSession from '../services/CreateSession';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const createSession = new CreateSession();
+  const createSession = new CreateSession();
 
-    const { user, token } = await createSession.execute({
-      email,
-      password,
-    });
+  const { user, token } = await createSession.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import './database';
+
 import appointmentsRouter from './routes/appointments.routes';
 import usersRouter from './routes/users.routes';
-import routes from './routes';
 import sessionsRouter from './routes/sessions.routes';
 import uploadConfig from './config/upload';
+
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -17,6 +20,22 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use('/sessions', sessionsRouter);
 app.use('/users', usersRouter);
 app.use('/appointments', appointmentsRouter);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
